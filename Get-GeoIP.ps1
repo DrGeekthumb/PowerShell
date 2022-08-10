4073be085fec73f51d67601f9dc2e241e803b5bc
Marc <76431323+Geekthumb@users.noreply.github.com>
2022-08-10 10:13:41 +0100

    Initial Commit

---
+++ Get-GeoIP.ps1
@@ -0,0 +1,12 @@
+<#
+Title: GeoIP Checker
+Version: 0.9
+Author: Geekthumb
+Info: Uses HackerTarget.com API to return geolocation results for a given IPv4 address.
+Usage: ./Get-GeoIP.ps1 <IPv4 address>
+#>
+
+$ip=$args[0]
+Write-Host " "
+curl https://api.hackertarget.com/geoip/?q=$ip -UseBasicParsing | Select-Object -Expand Content
+Write-Host " "
