a47b4d2026b54b41dfd417875f590188bb21c107
Marc <76431323+DrGeekthumb@users.noreply.github.com>
2023-07-27 18:38:45 +0100

    Update Info panel

--- Send2Discord.ps1
+++ Send2Discord.ps1
@@ -1,8 +1,10 @@
-# Title: Send2Discord
-# Author: DrGeekthumb
-# About: Uploads a file to your Discord server via a webhook. More info on webhooks can be found here: https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
-# Usage ./Send2Discord.ps1 "path/to/file"
-
+<#
+Title: Send2Discord
+Version: 1
+Author: DrGeekthumb
+Info: Uploads a file to your Discord server via a webhook. More info on webhooks can be found here: https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+Usage ./Send2Discord.ps1 "path/to/file"
+#>
 
 $file = $args[0]
 $url="WEBHOOK URL GOES HERER"
